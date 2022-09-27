86fe8b0eec579e194ed8d4239a7a2d78c1fc1462
wyl0929 <2241854054@qq.com>
2022-09-27 21:32:39 +0800

    modified reference & index

--- work/2_solution/program/data.m
+++
@@ -1,9 +0,0 @@
-%
-% Author       : Yulong Wang
-% Date         : 2021-11-08 20:20:21
-% LastEditors  : Yulong Wang
-% LastEditTime : 2022-03-18 20:11:23
-% FilePath     : \ŒÔ¿Ìc:\Users\WYL\Documents\template\work\2_solution\program\data.m
-% Description  :
-
-% input data
